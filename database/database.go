e1b42a3c81c92cf700140718f156be0f6ba7664c
codeninjaug <segayiandrew@gmail.com>
2022-03-12 21:49:16 +0300

    database configurations

--- database/database.go
+++ database/database.go
@@ -1,1 +1,33 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"log"
+	"time"
+	"go.mongodb.org/mongo-driver"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBinstance() *mongo.Client {
+	MongoDb := "mongodb://localhost:27017"
+	fmt.Println(MongoDb)
+	client , err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
+	if err!=nil{
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(),10*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("connected to mongodb")
+	return client
+}
+	var Client *mongo.Client = DBinstance()
+	func OpenCollection(client *mongo.Client,collectionName string) *mongo.Collection{
+		var collection *mongo.Collection = client.Database("restaurant").Collection(collectionName)
+		return collection
+	}
